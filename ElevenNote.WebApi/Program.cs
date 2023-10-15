6733231f67268122c11c2323fbb1e6f699731dda
Alex Westphal <alexelevenfifty@gmail.com>
2023-10-15 15:05:29 -0400

    Message

--- ElevenNote.WebApi/Program.cs
+++ ElevenNote.WebApi/Program.cs
@@ -1,8 +1,28 @@
+using ElevenNote.Data;
+using ElevenNote.Data.Entities;
+using ElevenNote.Services.User;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IUserService, UserService>();
+//Add Connection string and DbContext setup
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 
-builder.Services.AddControllers();
+builder.Services.AddDefaultIdentity<UserEntity>(options =>
+{
+    // Password configuration, optional
+    options.Password.RequiredLength = 4;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequireNonAlphanumeric = false;
+})
+    .AddRoles<IdentityRole<int>>() //ENable Roles, optional
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,6 +37,13 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+
+
+
+// Adds AuthenticationMiddleware to the IApplicationBuilder
+// This enables authentication capabilities
+app.UseAuthentication();
 
 app.UseAuthorization();
 
